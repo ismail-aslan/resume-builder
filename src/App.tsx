8781d5146f66aa6f5d0afd4495bc9ed4b6b6a819
Ismail Aslan <ismailaslan1097@gmail.com>
2024-03-20 22:42:39 +0100

    basic theme and locale setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { ConfigProvider, theme, Button, Card, Radio } from "antd";
+import type { ConfigProviderProps, RadioChangeEvent } from "antd";
+import enUS from "antd/locale/en_US";
+import deDE from "antd/locale/de_DE";
+
+type Locale = ConfigProviderProps["locale"];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const handleClick = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+  };
+  const [locale, setLocal] = useState<Locale>(enUS);
+
+  const changeLocale = (e: RadioChangeEvent) => {
+    const localeValue = e.target.value;
+    setLocal(localeValue);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ConfigProvider
+        locale={locale}
+        theme={{
+          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+        }}
+      >
+        <div style={{ marginBottom: 16 }}>
+          <span style={{ marginRight: 16 }}>Change locale of components:</span>
+          <Radio.Group value={locale} onChange={changeLocale}>
+            <Radio.Button key="en" value={enUS}>
+              English
+            </Radio.Button>
+            <Radio.Button key="cn" value={deDE}>
+              German
+            </Radio.Button>
+          </Radio.Group>
+        </div>
+        <Card style={{ width: "max-content" }}>
+          <Button onClick={handleClick}>
+            Change Theme to {isDarkMode ? "Light" : "Dark"}
+          </Button>
+        </Card>
+      </ConfigProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
